7c068cfb47ea66f5b3bd9aa4b0e5a0b20b61faf1
jujinhyeong <mhn03148@naver.com>
2022-09-08 00:25:06 +0900

    랜덤함수 생성, 배열저장

--- HW1_2017114657.c
+++ HW1_2017114657.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void sortFunction();
 
 int main(){
-    char c;
-    
-    scanf("%c", &c);
-
-    if(c=="1"){
-       printf("hello wold"); 
+    srand(time(NULL));
+    int vector_array[10];
+    for(int i =0; i<10; i++)
+    {
+        vector_array[i] = rand()%(-19)-10;
     }
+    for (int j=0; j<10; j++){
+        printf("%d \n",vector_array[j]);
+    }
+    return 0;
 }
