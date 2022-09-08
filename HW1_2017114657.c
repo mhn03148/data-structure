c79f6273676d5a1828f6575a51dfdc796d50e30d
jujinhyeong <mhn03148@naver.com>
2022-09-09 02:59:57 +0900

    quick sort

--- HW1_2017114657.c
+++ HW1_2017114657.c
@@ -5,6 +5,7 @@
 void sortFunction();
 
 int main(){
+
     srand(time(NULL));
     int vector_array[10];
     for(int i =0; i<10; i++)
@@ -15,4 +16,38 @@ int main(){
         printf("%d \n",vector_array[j]);
     }
     return 0;
+}
+
+void sortFunction(int *vector_array, int start, int end){
+    if(start>=end){
+        return;
+    }
+    int pivot = start;
+    int i = pivot+1;
+    int j = end;
+    int temp;
+
+    while(i<j){
+        while(i<end && vector_array[i]<=vector_array[pivot]){
+            ++i;
+        }
+        while(j>start && vector_array[j] >= vector_array[pivot]){
+            --j;
+        }
+        if (i>=j){
+            break;
+        }
+
+        temp = vector_array[i];
+        vector_array[i] = vector_array[j];
+        vector_array[j] = temp;
+    }
+
+    temp = vector_array[j];
+    vector_array[j] = vector_array[pivot];
+    vector_array[pivot] = temp;
+
+    sortFunction(vector_array, start, j-1);
+    sortFunction(vector_array, j+1, end);
+
 }
